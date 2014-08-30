9a03177e8a86be18f1b2105da1ef31d0dca17059
cfvbaibai <893963665@qq.com>
2014-08-30 16:20:28 +0800

    Initial commit

---
+++ What2Eat/src/What2EatCore/DB/tbl_dish.sql
@@ -0,0 +1,9 @@
+﻿USE What2Eat
+
+DROP TABLE tbl_dish
+CREATE TABLE tbl_dish (
+    guii_id         UNIQUEIDENTIFIER    NOT NULL PRIMARY KEY,
+    nvc_name        NVARCHAR(64)        NOT NULL,
+    si_type         SMALLINT            NOT NULL,
+    dt_updated      DATETIME            NOT NULL
+) ON [PRIMARY]
---
+++ What2Eat/src/What2EatCore/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cfvbaibai.What2Eat.Core
+{
+    public class User
+    {
+        public string ID { get; set; }
+        public string NickName { get; set; }
+        public string Password { get; set; }
+    }
+}
